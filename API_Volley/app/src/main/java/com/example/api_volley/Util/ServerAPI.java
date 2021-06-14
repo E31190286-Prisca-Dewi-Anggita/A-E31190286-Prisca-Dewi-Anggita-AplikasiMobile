0dae4e788c13b7df93e0d551146b465a30ec6f4c
E31190286-Prisca-Dewi-Anggita <priscadewi08@gmail>
2021-06-14 09:42:58 +0700

    E31190286

---
+++ API_Volley/app/src/main/java/com/example/api_volley/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.example.api_volley.Util;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://5cb1fbc1b1c2.ngrok.io/volley-crud-server/users";
+    public static final String URL_INSERT = "http://192.168.1.16/A_E31190286_Prisca-Dewi-Anggita_Web/Minggu12API/index.php/Mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://192.168.1.16/A_E31190286_Prisca-Dewi-Anggita_Web/Minggu12API/index.php/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE = "http://192.168.1.16/A_E31190286_Prisca-Dewi-Anggita_Web/Minggu12API/index.php/Mahasiswa/ApiUpdate";
+}
